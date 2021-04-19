6398cf3312bdb4b7f7461ee8aa540d23fe213213
JK <jkatzman@infsoftware.com>
2021-04-19 15:16:03 -0400

    add levels

--- src/game.js
+++ src/game.js
@@ -2,6 +2,7 @@ import Paddle from './paddle.js';
 import InputHandler from './input.js';
 import Ball from './ball.js';
 import Brick from './brick.js';
+import {buildLevel, level1} from './levels.js';
 
 export default class Game {
     constructor(gameWidth, gameHeight){
@@ -14,11 +15,13 @@ export default class Game {
         this.ball = new Ball(this);
         this.paddle = new Paddle(this);
         //let brick = new Brick(this, {x:20, y:20});
-        let bricks = [];
-for (let i = 0; i < 10; i++) {
-    bricks.push(new Brick(this, {x:i * 52, y:30})) ;
+
+        //let bricks=[];
+        let bricks = buildLevel(this, level1);
+// for (let i = 0; i < 10; i++) {
+//     bricks.push(new Brick(this, {x:i * 52, y:30})) ;
     
-}
+
 
 
         this.gameObjects = [
